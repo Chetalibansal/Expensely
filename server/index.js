6f4729fc562ebd01bf4f38d0a67019195e16bf2e
Chetalibansal <chetalibansal316@gmail.com>
2025-06-28 23:44:34 +0530

    feat: set up server with Express, CORS, and MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 import dotenv from 'dotenv'
-import {app} from 'app.js'
-
 dotenv.config({
     path: './.env'
 })
+import {app} from './app.js'
+import connectDB from './db/database.js'
 
+
+
+connectDB()
+.then(()=>{
+    app.on("error", (error)=>{
+        console.log("ERROR", error);
+        throw error;
+    });
+
+    app.listen(process.env.PORT || 3000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed!!! ", err);
+})
